31a911ea5363c3300296b8290574df37e2df5b49
Elena Kudina <botanik-lena@yandex.ru>
2022-12-17 20:33:37 +0300

    Добавлен инструмент Кисть

--- react-paint-app/src/components/Toolbar.jsx
+++ react-paint-app/src/components/Toolbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import '../styles/toolbar.scss';
+import toolState from "../store/toolState";
+import Brush from "../tools/Brush";
+import canvasState from "../store/canvasState";
 
 const Toolbar = () => {
+
+    const handleBrush = () => {
+        toolState.setTool(new Brush(canvasState.canvas))
+    };
     return (
         <div className="toolbar">
+            <button className="toolbar__btn toolbar__btn--brush" onClick={handleBrush}/>
+            <button className="toolbar__btn toolbar__btn--rect" />
+            <button className="toolbar__btn toolbar__btn--circle" />
+            <button className="toolbar__btn toolbar__btn--eraser" />
+            <button className="toolbar__btn toolbar__btn--line" />
+            <input type="color" className="toolbar__color-palette"/>
 
+            <button className="toolbar__btn toolbar__btn--undo" />
+            <button className="toolbar__btn toolbar__btn--redo" />
+            <button className="toolbar__btn toolbar__btn--save" />
         </div>
     );
 }
