ba76abedf13f93c17d05610361fcd02bb4c8452b
Elena Kudina <botanik-lena@yandex.ru>
2022-12-17 21:03:29 +0300

    Добавлен инструмент Прямоугольник

--- react-paint-app/src/components/Toolbar.jsx
+++ react-paint-app/src/components/Toolbar.jsx
@@ -3,16 +3,22 @@ import '../styles/toolbar.scss';
 import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
 import canvasState from "../store/canvasState";
+import Rect from "../tools/Rect";
 
 const Toolbar = () => {
 
     const handleBrush = () => {
         toolState.setTool(new Brush(canvasState.canvas))
     };
+
+    const handleRect = () => {
+        toolState.setTool(new Rect(canvasState.canvas))
+    };
+
     return (
         <div className="toolbar">
             <button className="toolbar__btn toolbar__btn--brush" onClick={handleBrush}/>
-            <button className="toolbar__btn toolbar__btn--rect" />
+            <button className="toolbar__btn toolbar__btn--rect" onClick={handleRect}/>
             <button className="toolbar__btn toolbar__btn--circle" />
             <button className="toolbar__btn toolbar__btn--eraser" />
             <button className="toolbar__btn toolbar__btn--line" />
